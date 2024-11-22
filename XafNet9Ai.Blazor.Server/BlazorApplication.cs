415be41a0712041f24f28bae398f3094c85aee49
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-11-22 13:24:12 +0300

    Add project files.

---
+++ XafNet9Ai.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using XafNet9Ai.Blazor.Server.Services;
+
+namespace XafNet9Ai.Blazor.Server;
+
+public class XafNet9AiBlazorApplication : BlazorApplication {
+    public XafNet9AiBlazorApplication() {
+        ApplicationName = "XafNet9Ai";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += XafNet9AiBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void XafNet9AiBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
---
+++ XafNet9Ai.Module/Controllers/embeddingGeneratorController.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics.Tensors;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafNet9Ai.Module.Controllers
+{
+    public class EmbeddingGeneratorController : ViewController
+    {
+        SimpleAction GeneratedEmbeddings;
+        public EmbeddingGeneratorController() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            GeneratedEmbeddings = new SimpleAction(this, "Generate Embeddings", "View");
+            GeneratedEmbeddings.Execute += GeneratedEmbeddings_Execute;
+            
+        }
+        private async void GeneratedEmbeddings_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = new OllamaEmbeddingGenerator(new Uri("http://127.0.0.1:11434"), modelId: "all-minilm:latest");
+            Embedding<float> result = await embeddingGenerator.GenerateEmbeddingAsync("XAF is the best application Framework");
+            Debug.WriteLine($"vector lenght:{result.Vector.Length}");
+
+            List<string> strings = new List<string>() { "hello", "bye" };
+
+            (string Value, Embedding<float> Embedding)[] ZipEmbeddings = await embeddingGenerator.GenerateAndZipAsync(strings);
+
+
+            var InputEmbedding = await embeddingGenerator.GenerateEmbeddingAsync("hello");
+
+
+            var Closest = from candidate in ZipEmbeddings
+                          let similarity = TensorPrimitives.CosineSimilarity(candidate.Embedding.Vector.Span, InputEmbedding.Vector.Span)
+                          orderby similarity descending
+                          select new { Text = candidate.Value, Similarity = similarity };
+                         
+
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112737/).
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+}
