48ba16c9f39508f2957534d3e83beca6bc5bc7cf
Jose Ojeda <joche.ojeda@bitframeworks.com>
2024-11-24 09:02:32 +0300

    Chat

---
+++ XafNet9Ai.Module/Controllers/ChatClientController.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafNet9Ai.Module.Controllers
+{
+    public class ChatClientController : ViewController
+    {
+        SimpleAction TestNewChatClient;
+        public ChatClientController() : base()
+        {
+            // Target required Views (use the TargetXXX properties) and create their Actions.
+            TestNewChatClient = new SimpleAction(this, "Test New Chat Client", "View");
+            TestNewChatClient.Execute += TestNewChatClient_Execute;
+            
+        }
+        private async void TestNewChatClient_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            IChatClient chatClient = new OllamaChatClient(new Uri("http://127.0.0.1:11434"), modelId: "phi3:latest");
+            Debug.WriteLine(await chatClient.CompleteAsync("What is A.I?"));
+
+            // Execute your business logic (https://docs.devexpress.com/eXpressAppFramework/112737/).
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+}
---
+++ XafNet9Ai.Module/LmStudioChatClient.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XafNet9Ai.Module
+{
+    //public class LmStudioChatClient : IChatClient
+    //{
+    //    public ChatClientMetadata Metadata => throw new NotImplementedException();
+
+    //    public Task<ChatCompletion> CompleteAsync(IList<ChatMessage> chatMessages, ChatOptions options = null, CancellationToken cancellationToken = default)
+    //    {
+
+    //        //var url = "http://localhost:1234/v1/completions"; // LM Studio server URL
+    //        //var apiKey = "your-api-key"; // Replace with your LM Studio API key
+
+    //        //var requestBody = new
+    //        //{
+    //        //    prompt = "Hello, how are you?",
+    //        //    max_tokens = 50
+    //        //};
+
+    //        //var json = Newtonsoft.Json.JsonConvert.SerializeObject(requestBody);
+    //        //var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+    //        //client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+    //        //var response = await client.PostAsync(url, content);
+    //        //var responseString = await response.Content.ReadAsStringAsync();
+
+    //        //Console.WriteLine(responseString);
+    //        //throw new NotImplementedException();
+    //    }
+
+    //    public IAsyncEnumerable<StreamingChatCompletionUpdate> CompleteStreamingAsync(IList<ChatMessage> chatMessages, ChatOptions options = null, CancellationToken cancellationToken = default)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public object GetService(Type serviceType, object serviceKey = null)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
